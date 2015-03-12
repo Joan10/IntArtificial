206407bd31e0e5aa77ed61f393b723945edd3f4c
jdeu <jdeu@jdeu-ThinkpadE130>
2015-03-12 16:57:56 +0100

    canvis 2

--- src/Bitxo1.java
+++ src/Bitxo1.java
@@ -2,12 +2,24 @@ package agents;
 
 // Exemple de Bitxo
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+
+
 public class Bitxo1 extends Agent
 {
     static final boolean DEBUG = false;
     
     boolean carril_trobat = false;
-
+    boolean jahedisparat = false;
+    
+    boolean replega=false; //Variable que serveix per comptar els passos de replegament del bitxo.
+    long aux_tmp=0; //Variable que serveix per comptar temps
     final int MEITAT = 400;
     
     final int PARET = 0;
@@ -22,8 +34,82 @@ public class Bitxo1 extends Agent
     Estat estat;
     int espera = 0;
 
-    
     long temps;
+    
+ public class distArray{
+        //Classe que manté una llista ordenada del recurs més proper amb el seu índex a la taula.
+     
+        List<Long> distList = new ArrayList<Long>();
+        List<Integer> indexList = new ArrayList<Integer>();
+        
+        
+        public distArray(){
+           
+        }
+        public void fica_element( long dist, int pos ){
+            
+            long dist_act;
+            int i = 0;
+            boolean found = false;
+            
+            if (distList.size() == 0) {
+                
+                distList.add(dist);
+                indexList.add(pos);              
+            }else{
+                for (i = 0; i<distList.size() && !found; i++){
+                    dist_act = distList.get(i);
+                    
+                    if (dist < dist_act){
+                        distList.add(i, dist);
+                        indexList.add(i, pos);
+                        
+                        found = true;
+                    }
+                }
+                if (!found){
+                    distList.add(i, dist);
+                    indexList.add(i, pos);
+                }
+            }
+            
+        }
+        
+        public int treuElement(int i){
+            return indexList.get(i);
+        }
+                       
+        
+    }
+
+    
+    private void miraBonificacio(){
+        //Funció que retorna el sector on es troba la bonificació més propera
+        
+        int i,mini = 0;
+        double dist,mindist = 0;
+        int distAnt;
+        distArray da = new distArray();
+        
+        
+        
+        for (i=0; i<estat.bonificacions.length; i++){
+            dist = estat.posicio.distancia(estat.bonificacions[i]);
+            da.fica_element((int)dist, i);
+        }
+        
+
+        distAnt = estat.distanciaVisor;
+        setDistanciaVisors((int)estat.bonificacions[da.treuElement(0)].distancia(estat.posicio));
+        mira(estat.bonificacions[da.treuElement(0)].x,estat.bonificacions[da.treuElement(0)].y);        
+        System.out.println(estat.objecteVisor[1]);
+        //if (estat.objecteVisor[1] != 0){
+           
+        //}
+        setDistanciaVisors(distAnt);
+        
+        
+    }
 
     public Bitxo1(Agents pare) {
         super(pare, "Bitxo1", "imatges/robotank2.gif");
@@ -36,6 +122,7 @@ public class Bitxo1 extends Agent
         setDistanciaVisors(350);
         setVelocitatLineal(5);
         setVelocitatAngular(5);
+        replega = false;
         espera = 0;
         temps = 0;
     }
@@ -45,8 +132,9 @@ public class Bitxo1 extends Agent
     {
         temps++;
         estat = estatCombat();
+
         
-        if (temps == 1 && !estat.veigEnemic){  
+       /* if (temps == 1 && !estat.veigEnemic){  
             //Giram tot d'una a l'esquerra per trobar l'enemic
             if (estat.posicio.x < MEITAT){
                 gira(-90);
@@ -74,11 +162,19 @@ public class Bitxo1 extends Agent
                 gira(90);
             }
             
-        }
+        }*/
                 
         if (espera > 0) {
             if (DEBUG) System.out.println("Espera "+espera);
             espera--;
+        }
+        else if (replega && temps - aux_tmp > 10){
+            gira(-90);
+            aux_tmp=temps;
+        }
+        else if (replega){
+            endavant();
+            replega = false;
         }
         else
         {
@@ -107,7 +203,27 @@ public class Bitxo1 extends Agent
                     if (estat.sector == 2 || estat.sector == 3)
                     {
                         mira(estat.posicioEnemic.x, estat.posicioEnemic.y);
-                        if (estat.impactesRival < 5) dispara();
+                        
+                        if (estat.balaEnemigaDetectada){
+                            //Si l'enemic ens dispara
+                            System.out.println("bala enemiga!");
+                            if (estat.impactesRebuts == 4) {
+                                hyperespai();
+                            }
+                            if (!jahedisparat){
+                                //Si l'enemic dispara primer partim!
+                                System.out.println("Pirat!!");
+                                replega = true;
+                                gira(90);
+                                endavant();
+                                aux_tmp = temps;
+                            }
+                        }
+                        
+                        if (estat.impactesRival < 5){
+                            jahedisparat = true;
+                            dispara();
+                        }
                     }
                     else if (estat.sector == 1)
                     {
@@ -118,8 +234,10 @@ public class Bitxo1 extends Agent
                         esquerra();
                     }
                 }else{
-                     setVelocitatLineal(2);
-                     setVelocitatAngular(2);
+                    // setVelocitatLineal(2);
+                     //setVelocitatAngular(2);
+                     jahedisparat = false;
+                     miraBonificacio();
 
                 }
 
